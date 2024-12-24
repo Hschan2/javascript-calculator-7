66a312de0d6c0302d002c48771a66736b33e39ae
Hchan2 <seongchan_@naver.com>
2024-12-24 22:41:44 +0900

    feat: 프로세스 진행

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+import { GetNumber } from "./calculate/getNumber";
+
 class App {
-  async run() {}
+  async run() {
+    const inputNumbers = await GetNumber();
+    const sum = await this.sumNumbers(inputNumbers);
+
+    MissionUtils.Console.print(`결과 : ${sum}`);
+  }
+
+  async sumNumbers(numbers) {
+    return numbers.reduce((acc, num) => acc + num, 0);
+  }
 }
 
 export default App;
